9a060bcddb08632c253bcb40aa6a8d5fb2b13f3a
Dan <Dan@DAN-PC>
2022-07-22 10:54:00 +0300

    Add project files.

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new List<Monkey>();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        // Online
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+        if (response.IsSuccessStatusCode)
+        {
+            var temp = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+            for (var i = 0; i < 100; i++)
+            {
+                foreach (var item in temp)
+                {
+                    monkeyList.Add(item);
+                }
+            }
+            //monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+
+        }
+
+        // Offline
+        //using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        //using var reader = new StreamReader(stream);
+        //var contents = await reader.ReadToEndAsync();
+        //monkeyList = JsonSerializer.Deserialize<List<Monkey>>(contents);
+
+        return monkeyList;
+    }
+}
